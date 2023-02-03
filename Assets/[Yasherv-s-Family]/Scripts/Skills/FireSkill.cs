135b7752559ba008a14889e64550e6f068de8675
mucahitaktrk-maigames <38586285+mucahitaktrk@users.noreply.github.com>
2023-02-03 23:34:19 +0300

    Skill

---
+++ Assets/[Yasherv-s-Family]/Scripts/Skills/FireSkill.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YashervsFamaily.Scripts.Skills;
+
+namespace YashervsFamaily.Scripts.Skills
+{
+    [CreateAssetMenu (fileName = "FireSkill",menuName = "Skills/FireSkill")]
+    public class FireSkill : SkillBase
+    {
+        public override void Skill()
+        {
+            
+        }
+    }
+}
+
---
+++ Assets/[Yasherv-s-Family]/Scripts/Skills/IceSkill.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YashervsFamaily.Scripts.Skills;
+
+namespace YashervsFamaily.Scripts.Skills
+{
+    [CreateAssetMenu (fileName = "IceSkill", menuName = "Skills/IceSkill")]
+    public class IceSkill : SkillBase
+    {
+        public override void Skill()
+        {
+            
+        }
+    }
+}
+
