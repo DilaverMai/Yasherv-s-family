577662e0a7e63e5912c8bd530d5c0f56d83f5fc6
mucahitaktrk-maigames <38586285+mucahitaktrk@users.noreply.github.com>
2023-02-05 13:40:40 +0300

    Fire

--- Assets/[Yasherv-s-Family]/Scripts/Obstacles/SpikeObstacle.cs
+++ Assets/[Yasherv-s-Family]/Scripts/Obstacles/SpikeObstacle.cs
@@ -5,26 +5,18 @@ using Character;
 using UnityEngine;
 using YashervsFamaily.Scripts.Character.Player;
 using YashervsFamaily.Scripts.SkillProgress;
-using YashervsFamaily.Scripts.Skills;
 
-public class FireObstacle : MonoBehaviour
+public class SpikeObstacle : MonoBehaviour
 {
-    private ParticleSystem _particleSystem;
-
-    private void Start()
-    {
-        _particleSystem = GetComponent<ParticleSystem>();
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent(out IceSkill iceSkill))
-        {
-            _particleSystem.Stop();
-        }
-        if(_particleSystem.isStopped) return;
         if (!other.TryGetComponent(out PlayerSkills playerSkills)) return;
         if (!other.TryGetComponent(out IDamageable damageable)) return;
+        if (SkillManager.Instance.IsShieldCollected)
+        {
+            playerSkills.shieldParticle.GetComponent<ParticleSystem>().Play();
+            return;
+        }
         damageable.TakeDamage(10);  
     }
 }
